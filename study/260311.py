ef91c9e12a6fa383de8efdffa59bf9cac1196fa7
Jun Hyeok Bae <62313706+JNavi10@users.noreply.github.com>
2026-03-12 10:32:21 +0900

    일단 됨

--- study/260311.py
+++ study/260311.py
@@ -16,7 +16,8 @@ def solve_new_min_max(new_arr = [1,2,3], old_arr = [4,5,6]):
                 continue
             # print(j,k)
             # 각 j 와 가능한 k 값 결합
-            new_min_j_k: int = old_arr[j] + new_arr[k]
+            #print(f"new_arr[{j}]={new_arr[j]}, old_arr[{k}]={old_arr[k]}")
+            new_min_j_k: int = new_arr[j] + old_arr[k]
             # arr값 임시 갱신
             if new_min > new_min_j_k or min_first_iter:
                 new_min = new_min_j_k
@@ -27,15 +28,26 @@ def solve_new_min_max(new_arr = [1,2,3], old_arr = [4,5,6]):
         # arr값 영구 갱신
         # print(f'min for {j} is {new_min}')
         solved_min_arr.append(new_min)
-        # print(f'max for {j} is {new_max}')
+        #print(f'max for {j} is {new_max}')
         solved_max_arr.append(new_max)
 
     return solved_min_arr, solved_max_arr
 
-print(solve_new_min_max())
+"""
+arr1 = [2, 0, 0]
+arr2 = [4 ,1, 1]
+arr3 = [1, 2, 6]
+max_state = arr1
+#min_state, _ = solve_new_min_max(input_arr, min_state)
+_, max_state = solve_new_min_max(arr2, max_state)
+print(max_state) # 6, 2, 1
+_, max_state = solve_new_min_max(arr3, max_state)
+print(max_state) # 7, 8, 8
+#print(solve_new_min_max())
+
 
 exit(123)
-
+"""
 
 N = int(input())
 
@@ -55,36 +67,9 @@ for i in range(N):
     # 새 input
     input_arr = [int(x) for x in input().split()]
 
-    new_min_state = min_state.copy()
-    new_max_state = max_state.copy()
+    min_state, _ = solve_new_min_max(input_arr, min_state)
+    _, max_state = solve_new_min_max(input_arr, max_state)
 
-    # j는  새 arr의 index
-    for j in range(3):
-        new_min = 0
-        new_max = 0
-        min_first_iter = True
-        max_first_iter = False
-        # k는 구 arr의 index
-        for k in range(3):
-            # index 차이 2 이상이면 skip
-            if abs(j-k) >= 2:
-                continue
-            # 각 j 와 가능한 k 값 결합
-            new_min_j_k = input_arr[j] + min_state[k]
-            new_max_j_k = input_arr[j] + min_state[k]
-            # arr값 임시 갱신
-            if new_min > new_min_j_k or min_first_iter:
-                new_min = new_min_j_k
-                min_first_iter = False
-            if new_max < new_max_j_k or max_first_iter:
-                new_max = new_max_j_k
-                max_first_iter = False
-        # arr값 영구 갱신
-        new_min_state[j] = new_min
-        new_max_state[j] = new_max
-
-    min_state = new_min_state.copy()
-    max_state = new_max_state.copy()
     
 
 print(max(max_state), min(min_state))

d7f6b985aa24b3eb610f74da0287fbf7ca489c0f
Jun Hyeok Bae <62313706+JNavi10@users.noreply.github.com>
2026-03-12 10:06:33 +0900

    min for 2 arrary working

--- study/260311.py
+++ study/260311.py
@@ -1,11 +1,37 @@
+# min
+
+arr_a = [1,2,3]
+arr_b = [4,5,6]
+
+# j는  새 arr의 index
+for j in range(3):
+    new_min = 0
+    new_max = 0
+    first_iter = True
+    # k는 구 arr의 index
+    for k in range(3):
+        # index 차이 2 이상이면 skip
+        if abs(j-k) >= 2:
+            continue
+        print(j,k)
+        # 각 j 와 가능한 k 값 결합
+        new_min_j_k = arr_b[j] + arr_a[k]
+        # arr값 임시 갱신
+        if new_min > new_min_j_k or first_iter:
+            new_min = new_min_j_k
+            first_iter = False
+    # arr값 영구 갱신
+    print(f'min for {j} is {new_min}')
+
+exit(123)
+
+
 N = int(input())
 
 sum = 0
 
 min_state = [0, 0, 0]
 max_state = [0, 0, 0]
-
-moveable = [-1,0,1]
 
 # i 는 입력 arr의 i번째 줄
 for i in range(N):
@@ -25,6 +51,8 @@ for i in range(N):
     for j in range(3):
         new_min = 0
         new_max = 0
+        min_first_iter = True
+        max_first_iter = False
         # k는 구 arr의 index
         for k in range(3):
             # index 차이 2 이상이면 skip
@@ -34,10 +62,12 @@ for i in range(N):
             new_min_j_k = input_arr[j] + min_state[k]
             new_max_j_k = input_arr[j] + min_state[k]
             # arr값 임시 갱신
-            if new_min > new_min_j_k or k==0:
+            if new_min > new_min_j_k or min_first_iter:
                 new_min = new_min_j_k
-            if new_max < new_max_j_k or k==0:
+                min_first_iter = False
+            if new_max < new_max_j_k or max_first_iter:
                 new_max = new_max_j_k
+                max_first_iter = False
         # arr값 영구 갱신
         new_min_state[j] = new_min
         new_max_state[j] = new_max

96155e532c2a27a505524a5fce3a4a4c793beb25
Jun Hyeok Bae <62313706+JNavi10@users.noreply.github.com>
2026-03-17 17:18:14 +0900

    11050

--- class 2/11050.py
+++ class 2/11050.py
@@ -1,5 +1,8 @@
-from itertools import combinations
-
 N, K = map(int, input().split())
 
-print()
+def factorial(i: int):
+    if i <= 1:
+        return 1
+    return i * factorial(i-1)
+
+print(factorial(N)//factorial(K)//factorial(N-K))

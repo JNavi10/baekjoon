6a32a4e358293cd54403f9296e543e46b7065ea8
Jun Hyeok Bae <62313706+JNavi10@users.noreply.github.com>
2026-04-01 17:59:32 +0900

    11650

---
+++ class 2+/11650.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+li = []
+for i in range(N):
+    li.append(tuple(map(int, input().split())))
+li.sort(key=lambda x: (x[0], x[1]))
+
+for i in li:
+    print(i[0], i[1])

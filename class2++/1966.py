7e087ed02a513ff7823b68d067ba4c86f123a13f
Jun Hyeok Bae <62313706+JNavi10@users.noreply.github.com>
2026-04-09 13:43:52 +0900

    1966

--- class2++/1966.py
+++ class2++/1966.py
@@ -2,6 +2,26 @@ test_case_count = int(input())
 
 for _ in range(test_case_count):
     N, curious = map(int, input().split())
-    Q = map(int, input().split())
+    Q = list(map(int, input().split()))
 
+    count = 0
+    def prioritize(Q, curious):
+        while True:
+            if Q[0] < max(Q):
+                Q.append(Q.pop(0))
+                curious -= 1
+                if curious < 0:
+                    curious += len(Q)
+            else:
+                break
+        return Q, curious
+    while Q:
+        Q, curious = prioritize(Q, curious)
+        #print('\t', Q, curious)
+        Q.pop(0)
+        curious -= 1
+        count += 1
+        if curious == -1:
+            break
+    print(count)
     

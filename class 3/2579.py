aaecaccb173bf89837ad7e295c83b69037376f62
Jun Hyeok Bae <62313706+JNavi10@users.noreply.github.com>
2026-04-14 17:59:23 +0900

    2579 wip

--- class 3/2579.py
+++ class 3/2579.py
@@ -5,6 +5,11 @@ sys.stdin.readline
 num_stairs = int(input())
 
 stairs = []
-for stair in range(num_stairs):
-    stairs.append(stair)
+for _ in range(num_stairs):
+    stairs.append(int(input()))
+
+cur_index = 0
+while cur_index < num_stairs:
+    
+    cur_index += 3
 
